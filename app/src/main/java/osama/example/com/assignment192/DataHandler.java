58149bc2810c70c0f74c22ab366a320fc59e94fa
Mohammad Osama <mdosama.ansar@gmail.com>
2017-09-23 13:05:44 +0530

    Android-Assignment-19.2
    included screenshot and apk file

---
+++ app/src/main/java/osama/example/com/assignment192/DataHandler.java
@@ -0,0 +1,32 @@
+package osama.example.com.assignment192;
+
+
+public class DataHandler {
+
+    int id, vote_count;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(int vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/osama/example/com/assignment192/network/OnWebServiceResult.java
@@ -0,0 +1,9 @@
+package osama.example.com.assignment192.network;
+
+
+import osama.example.com.assignment192.utils.CommonUtilities;
+
+
+public interface OnWebServiceResult {
+	public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
